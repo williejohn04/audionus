567e8c732c3dfc67a1aace60c403a5d96bf545dd
williejohn04 <johnwilliam1104@gmail.com>
2023-08-04 04:51:32 +0700

    add playlist detail page and some fixing

---
+++ routes/r_playlist.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/:id', ensureLoggedIn('/auth/login'), (req, res, next) => {
+    // https://api.deezer.com/playlist/1479458365
+    let data = {};
+    utils.hitDeezerAPI('/playlist/'+req.params.id, async (response) => {
+        if (response.errors) res.redirect('/');
+        
+        data.playlist = response[0]
+        data.title = data.playlist.title
+        if (req.query.layout == 'false') data.layout = false;
+        res.render('v_playlist/v_detail', data)
+    })
+})
+
+module.exports = router;
