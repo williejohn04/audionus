48a646c53eacde4b63780a46ad9885dfccdf4ca2
williejohn04 <johnwilliam1104@gmail.com>
2023-10-09 04:09:46 +0700

    fix here and there

--- routes/r_api.js
+++ routes/r_api.js
@@ -8,15 +8,45 @@ router.post('/get-splide-component', ensureLoggedIn('/auth/login'), (req, res, n
 });
 
 router.get('/search/:type', ensureLoggedIn('/auth/login'), (req, res, next) => {
-  utils.hitDeezerAPI('/search/'+req.params.type+'?limit=15&q='+decodeURIComponent(req.query.q), true, async (results) => {
+  utils.hitDeezerAPI('/search/'+req.params.type+'?limit=16&q='+decodeURIComponent(req.query.q), true, async (results) => {
     res.statusCode = 200;
     res.end(JSON.stringify(results[0]));
   })
 })
 
 router.post('/get-cards-component', ensureLoggedIn('/auth/login'), (req, res, next) => {
-  console.log(req.body.circleImg);
   res.render('partials/card_basic', {circleImg: req.body.circleImg, cardTitle: req.body.title, items: JSON.parse(req.body.json), type: req.body.type, layout: false});
+})
+
+router.get('/api/temp', (req, res, next) => {
+  const { dirname } = require('path');
+  res.sendFile(__dirname+'/Capture.PNG')
+})
+
+router.get('/radio/top', ensureLoggedIn('/auth/login'), async (req, res, next) => {
+    let radios = await axios.get('https://api.deezer.com/radio/top')
+    // let newImgUrl = '';
+    // for (let index = 0; index < radios.data.data.length; index++) {
+    //     newImgUrl = `https://textoverimage.moesif.com/image?image_url=${radios.data.data[index].picture_medium}&x_align=center&y_align=middle&text_size=16&margin=2&text=${radios.data.data[index].title}`;
+    //     radios.data.data[index].picture_medium = newImgUrl;
+    //     if (index == radios.data.data.length - 1) res.end(JSON.stringify(radios.data.data))
+    // }
+    res.end(JSON.stringify(radios.data.data))
+})
+
+router.get('/editorial/:type', ensureLoggedIn('/auth/login'), async (req, res, next) => {
+    let url = '';
+    
+    switch (req.params.type) {
+        case 'charts':
+            url = 'https://api.deezer.com/editorial/0/charts'
+            break;
+        case 'selection':
+            url = 'https://api.deezer.com/editorial/0/selection'
+            break;
+    }
+    const editorial = await axios.get(url);
+    res.end(JSON.stringify(editorial.data))
 })
 
 module.exports = router;
--- routes/r_main.js
+++ routes/r_main.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Playlist = require('../models/Playlist');
 
-router.get('/', ensureLoggedIn('/auth/login'), (req, res, next) => {
+
+router.get('/', ensureLoggedIn('/auth/login'), async (req, res, next) => {
   let data = {title: 'Home Page'};
   if (req.query.layout == 'false') data.layout = false;
-  res.render('index', data);
+  
+  data.chartsPlaylists = await Playlist.aggregate([
+    { $match: { type_playlist: 'charts' } },
+    { $sample: { size: 10 } }
+  ]);
+
+  data.chartsPlaylists.unshift({id: 3155776842, title: 'Top Worldwide', picture_medium: 'https://e-cdns-images.dzcdn.net/images/playlist/f1ac18441ab1dabc94282e4d1d5f4955/250x250-000000-80-0-0.jpg', type: 'playlist'}, {id: 1116188761, title: 'Top Indonesia', picture_medium: 'https://e-cdns-images.dzcdn.net/images/playlist/8bfe963147a8dff86df9ded5b90189c0/250x250-000000-80-0-0.jpg', type: 'playlist'})
+
+  res.render('index', data)
+  
 });
 
 module.exports = router;
